1fdc36a2be7ff08176b9f23bb4b7b307a0918022
Mark Zhou <149003514+markz03@users.noreply.github.com>
2026-05-01 16:03:14 +0000

    searchbar gitpages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import StatsPage from './components/StatsPage';
 import './App.css';
